09c5a86410ceafaf7eb5544e67f0e6952716cf0b
Thorin <thorin.iv@gmail.com>
2015-06-03 11:40:00 +0100

    Fixe 404 on add this project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 if Rails::VERSION::MAJOR >= 3
-	get 'favourite_projects/:action', :controller => 'favourite_projects'
+  get 'favourite_projects', :to => 'favourite_projects#index'
+  post 'favourite_projects/add_project', :to => 'favourite_projects#add_project'
+  post 'favourite_projects/remove_project', :to => 'favourite_projects#remove_project'
+
 else
   ActionController::Routing::Routes.draw do |map|
     map.connect 'favourite_projects/:action', :controller => 'favourite_projects'
